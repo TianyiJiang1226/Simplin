aff7f421d6ca428b35fb0d50441b52d4cc612e34
Your <your@email.com>
2024-02-01 21:53:45 -0700

    1

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 SimpLinR = function(X,Y){
   try(if(length(X) != length(Y)) stop('Nonequal vectors'))
-  if(length(X) == length(Y){SimpLinCpp(X,Y)
+  if(length(X) == length(Y)){SimpLinCpp(X,Y)
   }
 }
 
